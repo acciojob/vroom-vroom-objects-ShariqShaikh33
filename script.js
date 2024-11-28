f06db0d1e2850d41a9c26baeda8f4d9a99ae23da
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-28 14:42:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 // Complete the js code
+
 function Car(make, model) {
-	this.make= make;
+	this.make = make;
 	this.model = model;
-	getMakeModel(){
-		return (this.make+" "+this.model)
-	}
 }
+Car.prototype.getMakeModel = function() {
+		return (this.make+" "+this.model);
+	}
 
 function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
 	this.topSpeed = topSpeed;
-	getTopSpedd(){
-		return this.topSpeed;
-	}
+}
+SportsCar.prototype = Object.create(Car.prototype);
+
+SportsCar.prototype.getTopSpeed = function() {
+	return this.topSpeed;
 }
 
-SportsCar.__proto__ = Car;
+let car = new Car("Ferr","HErrr");
+console.log(car.getMakeModel());
 
 // Do not change the code below
 window.Car = Car;
